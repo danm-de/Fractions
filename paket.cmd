e4b273cb413cf4b1f7c630c752d9f7e8aec3bd79
Daniel Mueller <daniel@danm.de>
2017-04-30 11:56:30 +0200

    Using paket.bootstrapper.exe instead of paket.exe

--- paket.cmd
+++ paket.cmd
@@ -3,16 +3,8 @@
 set root=%~dp0
 set dir=%root%.paket
 set bootstrapper=%dir%\paket.bootstrapper.exe
-set paket=%dir%\paket.exe
 
-if not exist "%paket%" (
-  "%bootstrapper%"
-  if errorlevel 1 (
-    exit /b %errorlevel%
-  )
-)
-
-"%paket%" %*
+"%bootstrapper%" "--run" %*
 
 if errorlevel 1 (
   exit /b %errorlevel%
