74f41e0e7db4c078440815e66af23b6faced38bc
Daniel Mueller <daniel@danm.de>
2017-05-01 13:33:23 +0200

    Merge remote-tracking branch 'origin/master' into feature/dotnetstandard
    # Conflicts:
    #	Readme.md

---
+++ tests/Fractions.Json.Tests/Spec.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Fractions.Json.Tests
+{
+    public class Spec
+    {
+        [OneTimeSetUp]
+        public void TestFixtureSetUp() {
+            SetUp();
+            Arrange();
+            Act();
+        }
+
+        [OneTimeTearDown]
+        public void TestFixtureTearDown() {
+            TearDown();
+        }
+
+        public virtual void SetUp() { }
+
+        public virtual void TearDown() { }
+
+        public virtual void Arrange() { }
+
+        public virtual void Act() { }
+
+        protected Action Invoking(Action func) {
+            return func;
+        }
+    }
+}
