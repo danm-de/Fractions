db753c3748486f74f030fbfd7a67f636026a966a
Mads Foged <jfogedm@jci.com>
2022-02-17 15:25:44 +0100

    Added Sqrt() for Fraction

---
+++ src/Fractions/Fraction.Extensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System.Text;
+
+namespace Fractions {
+    public static class FractionExtensions
+    {
+
+        /// <summary>
+        /// Returns the square root of a fraction.
+        /// </summary>
+        public static Fraction Sqrt(this Fraction x) {
+            //Babylonian Method of computing square roots
+
+            if (x < 0) {
+                throw new OverflowException("Cannot calculate square root from a negative number");
+            }
+
+
+            Fraction oldGuess;
+            var newGuess = Fraction.Zero;
+            var tolerance = new Fraction(BigInteger.One, new BigInteger(79000000000000000000000000000m));
+
+
+            //Using Math.Sqrt to get a good starting guess
+            var guessDouble = Math.Sqrt((double)x);
+            if (double.IsInfinity(guessDouble)) {
+                oldGuess = x / 2;
+            } else {
+                oldGuess = (Fraction)guessDouble;
+            }
+
+
+            while ((oldGuess - newGuess).Abs() > tolerance) {
+                //Babylonian Method
+                newGuess = (oldGuess + (x / oldGuess)) / 2;
+                oldGuess = newGuess;
+            }
+
+            return newGuess;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ tests/Fractions.Tests/FractionSpecs/Sqrt/Method_Sqrt.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Tests.Fractions;
+
+namespace Fractions.Tests.FractionSpecs.Sqrt {
+
+    [TestFixture]
+    public class If_the_Sqrt_function_is_called : Spec {
+        [Test]
+        public void Sqrt_Of_1_Shall_Be_1() {
+            Fraction.One.Sqrt()
+                .Should().Be(1);
+        }
+
+        [Test]
+        public void Sqrt_Of_MinusOne_Shall_Be_Fail() {
+
+            Action act = () => Fraction.MinusOne.Sqrt();
+
+            act.Should().Throw<OverflowException>()
+                        .WithMessage("Cannot calculate square root from a negative number");
+        }
+
+        
+        private bool SqrtCompareToDouble(double testValue) {
+
+            var fractionSqrt = Fraction.FromDouble(testValue).Sqrt();
+            var doubleSqrt = Math.Sqrt(testValue);
+
+            return fractionSqrt.ToDouble() == doubleSqrt;            
+        }
+
+        [Test]
+        public void Sqrt_Of_values() {
+
+            SqrtCompareToDouble(0.001).Should().BeTrue();
+            SqrtCompareToDouble(5).Should().BeTrue();  
+            SqrtCompareToDouble(4.54854751).Should().BeTrue();
+            SqrtCompareToDouble(9999999855487).Should().BeTrue();
+            SqrtCompareToDouble(99999998554865557).Should().BeTrue();
+            SqrtCompareToDouble(Math.PI).Should().BeTrue();
+
+        }
+
+
+
+
+
+    }
+}
