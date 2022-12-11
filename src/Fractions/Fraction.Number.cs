2308852d89efd3c6a10b0205a96f330301e81bb2
Daniel Mueller <daniel@danm.de>
2022-12-11 16:05:33 +0100

    Start implementing INumber interface.

---
+++ src/Fractions/Fraction.Number.cs
@@ -0,0 +1,224 @@
+﻿#if NET7_0
+using System;
+using System.Globalization;
+using System.Numerics;
+
+namespace Fractions {
+    public partial struct Fraction : INumber<Fraction> {
+        /// <inheritdoc />
+        public static Fraction Parse(string s, IFormatProvider provider) =>
+            FromString(s, provider);
+
+        /// <inheritdoc />
+        public static Fraction Parse(string s, NumberStyles style, IFormatProvider provider) =>
+            FromString(s, style, provider);
+
+        /// <inheritdoc />
+        public static Fraction Parse(ReadOnlySpan<char> s, IFormatProvider provider) {
+            // TODO optimize FromString to use ReadOnlySpan (Split required https://github.com/dotnet/runtime/issues/76186)
+            return FromString(s.ToString(), provider);
+        }
+
+        /// <inheritdoc />
+        public static Fraction Parse(ReadOnlySpan<char> s, NumberStyles style, IFormatProvider provider) {
+            // TODO optimize FromString to use ReadOnlySpan (Split required https://github.com/dotnet/runtime/issues/76186)
+            return FromString(s.ToString(), style, provider);
+        }
+
+        /// <inheritdoc />
+        public static bool TryParse(string s, IFormatProvider provider, out Fraction result) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public bool TryFormat(Span<char> destination, out int charsWritten, ReadOnlySpan<char> format,
+            IFormatProvider provider) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool TryParse(ReadOnlySpan<char> s, IFormatProvider provider, out Fraction result) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool TryParse(ReadOnlySpan<char> s, NumberStyles style, IFormatProvider provider,
+            out Fraction result) {
+            throw new NotImplementedException();
+        }
+
+
+        /// <inheritdoc />
+        public static Fraction AdditiveIdentity => throw new NotImplementedException();
+
+        /// <inheritdoc />
+        public static Fraction operator --(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static Fraction operator ++(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static Fraction MultiplicativeIdentity => throw new NotImplementedException();
+
+        /// <inheritdoc />
+        public static Fraction operator -(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static Fraction operator +(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool IsCanonical(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool IsComplexNumber(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool IsEvenInteger(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool IsFinite(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool IsImaginaryNumber(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool IsInfinity(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool IsInteger(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool IsNaN(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        static bool INumberBase<Fraction>.IsNegative(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool IsNegativeInfinity(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool IsNormal(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool IsOddInteger(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        static bool INumberBase<Fraction>.IsPositive(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool IsPositiveInfinity(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool IsRealNumber(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool IsSubnormal(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        static bool INumberBase<Fraction>.IsZero(Fraction value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static Fraction MaxMagnitude(Fraction x, Fraction y) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static Fraction MaxMagnitudeNumber(Fraction x, Fraction y) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static Fraction MinMagnitude(Fraction x, Fraction y) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static Fraction MinMagnitudeNumber(Fraction x, Fraction y) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool TryConvertFromChecked<TOther>(TOther value, out Fraction result)
+            where TOther : INumberBase<TOther> {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool TryConvertFromSaturating<TOther>(TOther value, out Fraction result)
+            where TOther : INumberBase<TOther> {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool TryConvertFromTruncating<TOther>(TOther value, out Fraction result)
+            where TOther : INumberBase<TOther> {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool TryConvertToChecked<TOther>(Fraction value, out TOther result)
+            where TOther : INumberBase<TOther> {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool TryConvertToSaturating<TOther>(Fraction value, out TOther result)
+            where TOther : INumberBase<TOther> {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public static bool TryConvertToTruncating<TOther>(Fraction value, out TOther result)
+            where TOther : INumberBase<TOther> {
+            throw new NotImplementedException();
+        }
+
+
+
+        /// <inheritdoc />
+        public static int Radix => throw new NotImplementedException();
+    }
+}
+#endif
---
+++ tests/Fractions.Tests/FractionSpecs/Number/Methods_Number_Interface.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using NUnit.Framework;
+
+namespace Fractions.Tests.FractionSpecs.Number;
+
+[TestFixture]
+public class If_the_user_parses_string_to_fraction {
+    private static readonly CultureInfo De = CultureInfo.GetCultureInfo("de-DE");
+
+    private static IEnumerable<TestCaseData> TestCases {
+        get
+        {
+            yield return new TestCaseData("1")
+                .Returns(new Fraction(1,1));
+            yield return new TestCaseData("1/2")
+                .Returns(new Fraction(1,2));
+            yield return new TestCaseData("+1/2")
+                .Returns(new Fraction(1,2));
+            yield return new TestCaseData("+1/+2")
+                .Returns(new Fraction(1,2));
+            yield return new TestCaseData("1/+2")
+                .Returns(new Fraction(1,2));
+            yield return new TestCaseData("-1/2")
+                .Returns(new Fraction(-1,2));
+            yield return new TestCaseData("1/-2")
+                .Returns(new Fraction(-1,2));
+            yield return new TestCaseData("3,5")
+                .Returns(new Fraction(7,2));
+            yield return new TestCaseData("+3,5")
+                .Returns(new Fraction(7,2));
+            yield return new TestCaseData("-3,5")
+                .Returns(new Fraction(-7,2));
+        }
+    }
+
+    [Test,TestCaseSource(nameof(TestCases))]
+    public Fraction Should_it_return_the_expected_result_when_using_Parse_with_DE_culture(string value) =>
+        Fraction.Parse(value, De);
+
+    [Test,TestCaseSource(nameof(TestCases))]
+    public Fraction Should_it_return_the_expected_result_when_using_Parse_with_NumberStyles_and_DE_culture(string value) =>
+        Fraction.Parse(value, NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign, De);
+
+    [Test,TestCaseSource(nameof(TestCases))]
+    public Fraction Should_it_return_the_expected_result_when_using_Parse_with_ReadOnlySpan_and_DE_culture(string value) =>
+        Fraction.Parse(value.AsSpan(), De);
+
+    [Test,TestCaseSource(nameof(TestCases))]
+    public Fraction Should_it_return_the_expected_result_when_using_Parse_with_ReadOnlySpan_NumberStyles_and_DE_culture(string value) =>
+        Fraction.Parse(value.AsSpan(), NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign, De);
+}
