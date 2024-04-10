bc1b0eeda5536d1630bebddbc867fd484f968803
lipchev <lipchev@gmail.com>
2024-04-10 23:35:55 +0300

    introduced a set of benchmarks for the most common operations

---
+++ benchmarks/Fractions.Benchmarks/ConstructorBenchmarks.cs
@@ -0,0 +1,46 @@
+﻿using System.Numerics;
+using BenchmarkDotNet.Attributes;
+
+namespace Fractions.Benchmarks;
+
+[MemoryDiagnoser]
+public class ConstructorBenchmarks {
+    public static IEnumerable<int> IntegerValues => [0, 42, int.MinValue, int.MaxValue];
+    public static IEnumerable<decimal> DecimalValues => [0m, 42m, 123.45m, int.MinValue, decimal.MinValue, (decimal)Math.PI];
+    public static IEnumerable<double> DoubleValues => [0.0, 42, 123.45, (double)decimal.MinValue, double.MaxValue, Math.PI];
+
+    public IEnumerable<object[]> BigIntegerValues() 
+    {
+        yield return [new BigInteger(0), new BigInteger(1)];
+        yield return [new BigInteger(42), new BigInteger(1)];
+        yield return [new BigInteger(int.MinValue), new BigInteger(1)];
+        yield return [new BigInteger(int.MaxValue), new BigInteger(int.MaxValue)];
+        yield return [new BigInteger(decimal.MinValue), new BigInteger(int.MaxValue)]; // TODO check if this is reduced
+        yield return [new BigInteger(int.MaxValue), new BigInteger(double.MaxValue)];
+        yield return [new BigInteger(245850922), new BigInteger(78256779)]; // ~ PI
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(IntegerValues))]
+    public Fraction Construct_With_Int32(int value) {
+        return new Fraction(value);
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(DecimalValues))]
+    public Fraction Construct_With_Decimal(decimal value) {
+        return new Fraction(value);
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(DoubleValues))]
+    public Fraction Construct_With_Double(double value) {
+        return new Fraction(value);
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(BigIntegerValues))]
+    public Fraction Construct_With_BigIntegers(BigInteger numerator, BigInteger denominator) {
+        return new Fraction(numerator, denominator);
+    }
+}
---
+++ benchmarks/Fractions.Benchmarks/PowerMathBenchmarks.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+
+namespace Fractions.Benchmarks;
+
+[MemoryDiagnoser]
+[ShortRunJob]
+// [DryJob]
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+public class PowerMathBenchmarks {
+    public static IEnumerable<int> Powers => [0, 1, 2, -2, 50, 100];
+
+    public static IEnumerable<Fraction> FractionsToRaise => [2, 3, new Fraction(2, 3), 1024];
+
+    [ParamsSource(nameof(FractionsToRaise))]
+    public Fraction FractionToRaise { get; set; }
+
+
+    [Benchmark]
+    [BenchmarkCategory("Pow")]
+    [ArgumentsSource(nameof(Powers))]
+    public Fraction Power(int exponent) {
+        return Fraction.Pow(FractionToRaise, exponent);
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Sqrt")]
+    public Fraction Sqrt() {
+        return FractionToRaise.Sqrt();
+    }
+}
