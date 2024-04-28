71a5fdeec8bfc2179cc9da0d12f502306c31e8cd
Daniel Mueller <daniel@danm.de>
2024-04-28 23:18:31 +0200

    Reformat code.
    Simplify IsZero property (remove duplicate condition)

--- src/Fractions/Fraction.cs
+++ src/Fractions/Fraction.cs
@@ -13,9 +13,44 @@ namespace Fractions;
 [TypeConverter(typeof(FractionTypeConverter))]
 [StructLayout(LayoutKind.Sequential)]
 public readonly partial struct Fraction : IEquatable<Fraction>, IComparable, IComparable<Fraction>, IFormattable {
-    private static readonly BigInteger TEN = new (10);
+    private static readonly BigInteger TEN = new(10);
 
     private readonly BigInteger? _denominator;
+
+    /// <summary>
+    /// A fraction representing the positive infinity.
+    /// </summary>
+    public static Fraction PositiveInfinity { get; } = new(BigInteger.One, BigInteger.Zero, FractionState.IsNormalized);
+
+    /// <summary>
+    /// A fraction representing the negative infinity.
+    /// </summary>
+    public static Fraction NegativeInfinity { get; } = new(BigInteger.MinusOne, BigInteger.Zero, FractionState.IsNormalized);
+
+    /// <summary>
+    /// A fraction representing the result of dividing zero by zero.
+    /// </summary>
+    public static Fraction NaN { get; } = new(BigInteger.Zero, BigInteger.Zero, FractionState.IsNormalized);
+
+    /// <summary>
+    /// A fraction with the reduced/simplified value of 0.
+    /// </summary>
+    public static Fraction Zero { get; } = new(BigInteger.Zero, BigInteger.One, FractionState.IsNormalized);
+
+    /// <summary>
+    /// A fraction with the reduced/simplified value of 1.
+    /// </summary>
+    public static Fraction One { get; } = new(BigInteger.One, BigInteger.One, FractionState.IsNormalized);
+
+    /// <summary>
+    /// A fraction with the reduced/simplified value of 2.
+    /// </summary>
+    public static Fraction Two { get; } = new(new BigInteger(2), BigInteger.One, FractionState.IsNormalized);
+
+    /// <summary>
+    /// A fraction with the reduced/simplified value of -1.
+    /// </summary>
+    public static Fraction MinusOne { get; } = new(BigInteger.MinusOne, BigInteger.One, FractionState.IsNormalized);
 
     /// <summary>
     /// The numerator.
@@ -31,17 +66,17 @@ public readonly partial struct Fraction : IEquatable<Fraction>, IComparable, ICo
     /// <c>true</c> if the fraction represents a valid number or <c>false</c> otherwise.
     /// </summary>
     public bool IsNaN => Denominator.IsZero && Numerator.IsZero;
-    
+
     /// <summary>
     /// <c>true</c> if the fraction evaluates to positive or negative infinity or <c>false</c> otherwise.
     /// </summary>
     public bool IsInfinity => Denominator.IsZero && !Numerator.IsZero;
-    
+
     /// <summary>
     /// <c>true</c> if the fraction evaluates to positive infinity or <c>false</c> otherwise.
     /// </summary>
     public bool IsPositiveInfinity => Denominator.IsZero && Numerator.Sign == 1;
-    
+
     /// <summary>
     /// <c>true</c> if the fraction evaluates to negative infinity or <c>false</c> otherwise.
     /// </summary>
@@ -68,45 +103,10 @@ public readonly partial struct Fraction : IEquatable<Fraction>, IComparable, ICo
     /// <summary>
     /// <c>true</c> if the fraction represents the value 0 or <c>false</c> otherwise.
     /// </summary>
-    public bool IsZero => Numerator.IsZero && !IsNaN;
+    public bool IsZero => Numerator.IsZero && !Denominator.IsZero;
 
     /// <summary>
     /// The fraction's state.
     /// </summary>
     public FractionState State { get; }
-
-    /// <summary>
-    /// A fraction representing the positive infinity.
-    /// </summary>
-    public static Fraction PositiveInfinity { get; } = new(BigInteger.One, BigInteger.Zero, FractionState.IsNormalized);
-
-    /// <summary>
-    /// A fraction representing the negative infinity.
-    /// </summary>
-    public static Fraction NegativeInfinity { get; } = new(BigInteger.MinusOne, BigInteger.Zero, FractionState.IsNormalized);
-
-    /// <summary>
-    /// A fraction representing the result of dividing zero by zero.
-    /// </summary>
-    public static Fraction NaN { get; } = new(BigInteger.Zero, BigInteger.Zero, FractionState.IsNormalized);
-
-    /// <summary>
-    /// A fraction with the reduced/simplified value of 0.
-    /// </summary>
-    public static Fraction Zero { get; } = new (BigInteger.Zero, BigInteger.One, FractionState.IsNormalized);
-
-    /// <summary>
-    /// A fraction with the reduced/simplified value of 1.
-    /// </summary>
-    public static Fraction One { get; } = new(BigInteger.One, BigInteger.One, FractionState.IsNormalized);
-
-    /// <summary>
-    /// A fraction with the reduced/simplified value of 2.
-    /// </summary>
-    public static Fraction Two { get; } = new(new BigInteger(2), BigInteger.One, FractionState.IsNormalized);
-
-    /// <summary>
-    /// A fraction with the reduced/simplified value of -1.
-    /// </summary>
-    public static Fraction MinusOne { get; } = new(BigInteger.MinusOne, BigInteger.One, FractionState.IsNormalized);
 }
