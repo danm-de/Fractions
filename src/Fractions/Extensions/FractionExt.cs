c2cd8dd69847ccd556f52b69ee33a7c9755cb05a
lipchev <lipchev@gmail.com>
2024-11-19 17:57:57 +0200

    improved the doc, adding a note about rounding the result

--- src/Fractions/Extensions/FractionExt.cs
+++ src/Fractions/Extensions/FractionExt.cs
@@ -10,12 +10,20 @@ namespace Fractions;
 /// </summary>
 public static class FractionExt {
     /// <summary>
-    /// Returns the square root of <paramref name="x"/>.
-    /// Use <paramref name="accuracy"/> to set the accuracy by specifying the number of digits after the decimal point of accuracy.
-    /// Higher value of <paramref name="accuracy"/> means better accuracy but longer calculations time.
+    ///     Returns the square root of <paramref name="x" />.
+    ///     Use <paramref name="accuracy" /> to set the accuracy by specifying the number of digits after the decimal point of
+    ///     accuracy.
+    ///     Higher value of <paramref name="accuracy" /> means better accuracy but longer calculations time.
     /// </summary>
     /// <param name="x">Source value</param>
     /// <param name="accuracy">Number of digits after the decimal point of accuracy</param>
+    /// <returns>The square root of the given fraction</returns>
+    /// <remarks>
+    ///     The resulting fraction will have at least, but likely a lot more, digits than the specified accuracy.
+    ///     If the result is used in further arithmetic operations, it is strongly recommended that the fraction is first
+    ///     rounded to the specified accuracy.
+    /// </remarks>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="accuracy" /> is less than or equal to zero</exception>
     public static Fraction Sqrt(this Fraction x, int accuracy = 30) {
         //Babylonian Method of computing square roots
 
