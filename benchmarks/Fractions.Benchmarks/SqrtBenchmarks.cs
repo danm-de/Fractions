a6c11b24b21e96363a548c75e05cbada6aade22c
lipchev <lipchev@gmail.com>
2025-01-30 01:56:44 +0200

    Re-implement the Sqrt function (fixes #97): - implementation based on https://github.com/SunsetQuest/NewtonPlus-Fast-BigInteger-and-BigFloat-Square-Root - the accuracy parameter switched from "number of decimals" to "number of digits" - completed the test coverage and added additional benchmarks

---
+++ benchmarks/Fractions.Benchmarks/SqrtBenchmarks.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Fractions.Benchmarks;
+
+[MemoryDiagnoser]
+public class SqrtBenchmarks {
+    public static IEnumerable<Fraction> FractionsToTest => [
+        2, 3, new(2, 3), 12.345m, 9187.45m, 1024, 123456789L * 123456789L, 123456789L * 123456789L + 1
+    ];
+
+    [Params(15, 30, 45, 90)]
+    public int Accuracy { get; set; }
+
+    [ParamsSource(nameof(FractionsToTest))]
+    public Fraction Fraction { get; set; }
+
+
+    [Benchmark(Baseline = true)]
+    public Fraction Sqrt() {
+        return Fraction.Sqrt(Accuracy);
+    }
+}
